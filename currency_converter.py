a2591648412d21f1ff75dffc2c6a4645db564970
cjredmond <connor.redmond@gmail.com>
2016-09-26 16:16:46 -0400

    before reducing covert method to 1 parameter

--- currency_converter.py
+++ currency_converter.py
@@ -1,34 +1,92 @@
-# 1 dollar  100.25 JPY
-#           .89 EUR
-#           .0016 BTC
-#1 Euro     1.13 USD
-#1 Yen      .01 USD
+conversion = {
+    'USD': 1,
+    'EUR': 1.125502,
+    'JPY': .009966,
+    'BTC': 605.79
+}
 
+back_conversion = {
+    'USD': 1,
+    'EUR': .888629,
+    'JPY': 100.339381,
+    'BTC': .00166
 
+}
 
 class Money:
-    def __init__(self, symbol, amount):
+
+    def __init__(self, symbol, value):
         self.symbol = symbol
-        self.amount = amount
+        self.value = value
+
+    def convert(self, other):
+        self.value = self.value * conversion[self.symbol]
+        other.value = other.value * conversion[other.symbol]
 
     def __add__(self, other):
-        if other.symbol == "EUR":
-            return Money(self.symbol, self.amount + (other.amount * 1.13))
-        elif other.symbol == "JPY":
-            return Money(self.symbol, self.amount + (other.amount * .01))
-        elif other.symbol == "BTC":
-            return Money(self.symbol, self.amount + (other.amount *.0016))
+        self.convert(other)
+        return Money(self.symbol, (self.value + other.value) * back_conversion[self.symbol])
+
+    def __sub__(self, other):
+        self.convert(other)
+        return Money(self.symbol, (self.value - other.value) * back_conversion[self.symbol])
+
+    def __mul__(self, other):
+        self.convert(other)
+        return Money(self.symbol, (self.value * other.value) * back_conversion[self.symbol])
+
+    def __str__(self):
+        return "{} {}".format(self.symbol, self.value)
+
+    def __lt__(self, other):
+        self.convert(other)
+        if self.value < other.value:
+            return True
         else:
-            return Money(self.symbol ,self.amount + other.amount)
+            return False
+
+    def __le__(self, other):
+        self.convert(other)
+        if self.value <= other.value:
+            return True
+        else:
+            return False
+
+    def __eq__(self, other):
+        self.convert(other)
+        if self.value == other.value:
+            return True
+        else:
+            return False
+
+    def __ne__(self, other):
+        self.convert(other)
+        if self.value != other.value:
+            return True
+        else:
+            return False
+
+    def __gt__(self, other):
+        self.convert(other)
+        if self.value > other.value:
+            return True
+        else:
+            return False
+
+    def __ge__(self, other):
+        self.convert(other)
+        if self.value >= other.value:
+            return True
+        else:
+            return False
+
+
+
+print(Money("BTC", 1) - Money("USD", 1) + Money("EUR", 1))
+print(Money("USD", 100) == Money("EUR", 20))
 
 
 
 
-first = Money("USD",100)
-second = Money("EUR", 200)
-x = first + second
-print(x.amount)
-y = Money("USD", 200) + Money("JPY", 1000)
-print(y.amount)
-z = Money("USD", 100) + Money("BTC", 100000) + Money("EUR", 100) + Money("USD", 100)
-print(z.amount)
+print(Money("JPY", 1000) + Money("USD", 1))
+print(Money("USD", 100) == Money("EUR", 20))
