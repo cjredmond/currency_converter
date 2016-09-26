33ef71df0c5ef47dd31d96e239133351aea5f866
cjredmond <connor.redmond@gmail.com>
2016-09-26 16:25:39 -0400

    reduced convert method

--- currency_converter.py
+++ currency_converter.py
@@ -19,74 +19,61 @@ class Money:
         self.symbol = symbol
         self.value = value
 
-    def convert(self, other):
-        self.value = self.value * conversion[self.symbol]
-        other.value = other.value * conversion[other.symbol]
+    def convert(self):
+        return self.value * conversion[self.symbol]
 
     def __add__(self, other):
-        self.convert(other)
-        return Money(self.symbol, (self.value + other.value) * back_conversion[self.symbol])
+        return Money(self.symbol, (self.convert() + other.convert()) * back_conversion[self.symbol])
 
     def __sub__(self, other):
-        self.convert(other)
-        return Money(self.symbol, (self.value - other.value) * back_conversion[self.symbol])
+        return Money(self.symbol, (self.convert() - other.convert()) * back_conversion[self.symbol])
 
     def __mul__(self, other):
-        self.convert(other)
-        return Money(self.symbol, (self.value * other.value) * back_conversion[self.symbol])
+        return Money(self.symbol, (self.convert() * other.convert()) * back_conversion[self.symbol])
 
     def __str__(self):
         return "{} {}".format(self.symbol, self.value)
 
     def __lt__(self, other):
-        self.convert(other)
-        if self.value < other.value:
+        if self.convert() < other.convert():
             return True
         else:
             return False
 
     def __le__(self, other):
-        self.convert(other)
-        if self.value <= other.value:
+        if self.convert() <= other.convert():
             return True
         else:
             return False
 
     def __eq__(self, other):
-        self.convert(other)
-        if self.value == other.value:
+        if self.convert() == other.convert():
             return True
         else:
             return False
 
     def __ne__(self, other):
-        self.convert(other)
-        if self.value != other.value:
+        if self.convert() != other.convert():
             return True
         else:
             return False
 
     def __gt__(self, other):
-        self.convert(other)
-        if self.value > other.value:
+        if self.convert() > other.convert():
             return True
         else:
             return False
 
     def __ge__(self, other):
-        self.convert(other)
-        if self.value >= other.value:
+        if self.convert() >= other.convert():
             return True
         else:
             return False
 
 
 
-print(Money("BTC", 1) - Money("USD", 1) + Money("EUR", 1))
-print(Money("USD", 100) == Money("EUR", 20))
-
-
-
-
-print(Money("JPY", 1000) + Money("USD", 1))
-print(Money("USD", 100) == Money("EUR", 20))
+print(Money("EUR", 1000) - Money("USD", 1))
+print(Money("BTC", 1000) + Money("JPY", 100101))
+print(Money("JPY", 1000) > Money("USD", 1))
+print(Money("JPY", 1000) <= Money("USD", 1))
+print(Money("JPY", 1000) != Money("USD", 1))
